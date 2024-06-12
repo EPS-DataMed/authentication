ba31bddd249a4d3852fdf3904e36ebae97d37205
VictorAmaralC <victoramaralce@gmail.com>
2024-06-12 19:15:50 -0300

    Retirando variáveis do código
    Retirando variáveis do sistema do código

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,6 @@
+import os
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -12,9 +14,20 @@ from .schemas import userSchema
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY não encontrada. Verifique o arquivo .env.")
+
+if not ALGORITHM:
+    raise ValueError("ALGORITHM não encontrada. Verifique o arquivo .env.")
+
+if not ACCESS_TOKEN_EXPIRE_MINUTES:
+    raise ValueError("ACCESS_TOKEN_EXPIRE_MINUTES não encontrada. Verifique o arquivo .env.")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
