d9c9629e28cdcbb5f71b11737005b3d332f3359a
VictorAmaralC <victoramaralce@gmail.com>
2024-06-13 20:50:23 -0300

    fix: login route

--- app/oauth2.py
+++ app/oauth2.py
@@ -31,7 +31,7 @@ if not ACCESS_TOKEN_EXPIRE_MINUTES:
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -17,7 +17,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
     
-    if not utils.verify_password(user_credentials.password, user.senha):
+    if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
     
     access_token = oauth2.create_access_token(data={"user_id": user.id})
