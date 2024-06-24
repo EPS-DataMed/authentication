afcf2eb4de5e9c84f43fc6046f50fb07a6e8d595
vitordiniz25 <vitordiniz25@gmail.com>
2024-06-24 19:26:40 -0300

    feat: create endpoint for validate email token

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
+import base64
+import jwt
+from datetime import datetime
 import requests
 import os
 from fastapi import HTTPException
@@ -32,3 +35,20 @@ def encrypt_password(password: str) -> str:
         raise HTTPException(status_code=500, detail="Error encrypting password")
     
     return response.json()["encrypted_message"]
+
+def decode_and_validate_token(token: str):
+    try:
+        padding = '=' * (4 - (len(token) % 4))
+        url_safe_token = token + padding
+        
+        jwt_token = base64.urlsafe_b64decode(url_safe_token.encode()).decode()
+        
+        token_data = jwt.decode(jwt_token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")])
+        
+        expires = datetime.strptime(token_data["expires"], '%Y-%m-%d %H:%M:%S.%f')
+        if datetime.utcnow() > expires:
+            return False
+        
+        return True
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, Exception):
+        return False
