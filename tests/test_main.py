c7f8955e9107fa7dc8c4e84ade64d12aedd96ebc
VictorAmaralC <victoramaralce@gmail.com>
2024-07-03 00:28:10 -0300

    feat/testes
    testes de rotas

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,127 @@
-def test_dummy():
-    assert (1+1) == 2
+from datetime import datetime
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.database import Base, get_db
+from app.main import app
+from app.models import userModel
+from app.utils import encrypt_password
+from app.oauth2 import create_access_token
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+# Função para obter a sessão de banco de dados para os testes
+@pytest.fixture
+def db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Configuração do cliente de teste
+@pytest.fixture
+def client():
+    def override_get_db():
+        try:
+            db = TestingSessionLocal()
+            yield db
+        finally:
+            db.rollback()
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+# Função de criação de usuário para testes
+def create_test_user(db):
+    password = "test_password"
+    hashed_password = encrypt_password(password)
+    birth_date = datetime.strptime("2000-01-01", "%Y-%m-%d").date()
+    user = userModel.User(
+        id=2,
+        full_name="Test User",
+        email="testes@example.com",
+        password=hashed_password,
+        birth_date=birth_date,
+        biological_sex="M"
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+# Teste para criação de usuário
+def test_create_user(client, db):
+    new_user = {
+        "full_name": "Test User",
+        "email": "test@example.com",
+        "password": "test_password",
+        "birth_date": "2000-01-01",
+        "biological_sex": "M"
+    }
+    response = client.post("/auth/users", json=new_user)
+    assert response.status_code == 200
+    assert response.json()["email"] == new_user["email"]
+
+# Teste para comparação de senha
+def test_compare_password(client, db):
+    user = create_test_user(db)
+    password_data = {
+        "password": "test_password"
+    }
+    response = client.post(f"/auth/users/{user.id}/compare-password", json=password_data)
+    assert response.status_code == 200
+    assert response.json() is True
+    db.delete(user)
+    db.commit()
+
+# Teste para atualização de senha
+def test_update_password(client, db):
+    user = create_test_user(db)
+    new_password = {
+        "password": "new_test_password"
+    }
+    response = client.put(f"/auth/users/{user.id}/password", json=new_password)
+    assert response.status_code == 200
+    assert response.json()["id"] == user.id
+    db.delete(user)
+    db.commit()
+
+# Teste para recuperação de senha
+def test_forgot_password(client, db):
+    user = create_test_user(db)
+    email_data = {
+        "email": user.email
+    }
+    response = client.post("/auth/forgot-password", json=email_data)
+    assert response.status_code == 200
+    assert "Enviamos um e-mail com as instruções para redefinir sua senha." in response.json()["message"]
+    db.delete(user)
+    db.commit()
+
+# Teste para criação de médico
+def test_create_doctor(client, db):
+    new_user = create_test_user(db)
+    new_doctor = {
+        "user_id": 1,
+        "crm": "12345",
+        "specialty": "Cardiologia"
+    }
+    response = client.post("/auth/doctors", json=new_doctor)
+    assert response.status_code == 200
+    db.delete(new_user)
+    db.commit()
+
+# Teste de rota de autenticação: /auth/login
+def test_login(client):
+    response = client.post("/auth/login", data={"username": "test@example.com", "password": "test_password"})
+    assert response.status_code == 200
